355f50140f98ba29ff3dbff9569571e939d0878d
volvap <vladislav98@tyt.by>
2020-03-25 15:42:57 +0300

    Change countries list

--- covid-19_cis.py
+++ covid-19_cis.py
@@ -4,14 +4,14 @@ import re
 
 
 page_link = "https://en.wikipedia.org/wiki/2019%E2%80%9320_coronavirus_pandemic"
-countries = ['Belarus','Ukraine','Russia','Italy','Spain','Germany']
+countries = ['Belarus','Ukraine','Russia']
 
 def wiki_parsing(main_str):
     get_request = requests.get(page_link)
     pattern_string = r"(?<="+ main_str + r"\").\b.{1,200}\b"
     cis_countries = re.search(pattern_string,str(get_request.content))
     temp_content = str(cis_countries.group(0))
-    cis_digit = re.search(r"(?<=;\">).(?:(\d{1,1000},\d{1,1000})|\d{1,1000})",temp_content)
+    cis_digit = re.search(r"(?<=n<td>).\d{1,10}",temp_content)
     print(f"In {main_str} right now infected {cis_digit.group(0)} people")
 
 if __name__ == '__main__':
