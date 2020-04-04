cf9f1322235ee3708b315045be9054faa1ee9619
Vladislav Pavlov <48180431+volvap@users.noreply.github.com>
2020-04-04 20:04:53 +0300

    Add files via upload

--- covid-19_cis.py
+++ covid-19_cis.py
@@ -6,8 +6,7 @@ import re
 page_link = "https://en.wikipedia.org/wiki/2019%E2%80%9320_coronavirus_pandemic"
 countries = ['Belarus','Ukraine','Russia','Poland','Lithuania']
 
-def wiki_parsing(main_str):
-    get_request = requests.get(page_link)
+def wiki_parsing(main_str,get_request):
     pattern_string = r"(?<="+ main_str + r"\").\b.{1,200}\b"
     cis_countries = re.search(pattern_string,str(get_request.content))
     temp_content = str(cis_countries.group(0))
@@ -15,6 +14,7 @@ def wiki_parsing(main_str):
     print(f"In {main_str} right now infected {cis_digit.group(0)} people")
 
 if __name__ == '__main__':
+    get_request = requests.get(page_link)
     for i in countries:
-        x = threading.Thread(target=wiki_parsing,args=(i,))
+        x = threading.Thread(target=wiki_parsing,args=(i,get_request))
         x.start()
